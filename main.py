95ff08434555c7f7dc3418df4fe5c0a18c41b624
tcaf09 <tcaffery09@gmail.com>
2026-09-11 15:08:23 +1000

    Added args

--- main.py
+++ main.py
@@ -13,12 +13,37 @@ from bs4 import BeautifulSoup
 import os
 from google import genai
 from faker import Faker
+import argparse
 
 fake = Faker()
 width = shutil.get_terminal_size().columns
 client = genai.Client()
 console = Console()
 KEYBINDS = Text("[Q]uit     [S]ummarize page", style="bold black on white")
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="qsearch",
+        description="Quick terminal search and summarization"
+    )
+    parser.add_argument('query', help="Search query")
+    parser.add_argument(
+        "-n", "--num-results",
+        type=int,
+        default=5,
+        help="Number of results to show (Default 5)"
+    )
+    parser.add_argument(
+        "--quick",
+        action="store_true",
+        help="Quick AI answer"
+    )
+    parser.add_argument(
+        "--site",
+        default=None,
+        help="Filter results by site"
+    )
+    return parser.parse_args()
 
 class StatusBar:
     def __init__(self, console):
@@ -47,17 +72,40 @@ class StatusBar:
 
 status_bar = StatusBar(console)
 
+def pickUrl(results):
+    urls = map(lambda res: res['href'], results)
+    status_bar.stop()
+    url = questionary.select(
+        "Select Url: ",
+        list(urls)
+    ).ask()
+    status_bar.start()
+    return url
 
 def main():
-    status_bar.start()
+    args = parse_args()
 
-    if len(sys.argv) != 2:
-        console.print("[bold red]No query provided[/bold red]")
-        status_bar.stop()
+    query = args.query
+    if args.quick:
+        with console.status("Pondering...", spinner="arc"):
+            interaction = client.interactions.create(
+                model='gemini-3.8-flash',
+                input=f'''
+                You are an expert at answering any question or summarizing any topic provided in minimal words while still retaining all the key information.
+                Always output in a form the can be processed by the rich python library. This means surronding text in appropriate colors or decorations.
+                You must always put closing tags for any decorated text. For example, the response must look like [options]Text here[/options], where options is replaced with colors like red or blue or font decorations like bold or italic.
+                You may add mutiple options to this rich text.
+                Answer this question or summarize this topic: {args.query}
+                '''
+            )
+        console.print(interaction.output_text)
         return
 
-    query = sys.argv[1]
-    results = search(query)
+    status_bar.start()
+    if args.site:
+        query = f'site:{args.site} {query}'
+
+    results = search(query, args.num_results)
 
     for res in results:
         console.print(f'[bold blue]{res['title']}[/bold blue]')
@@ -71,34 +119,15 @@ def main():
     while True:
         key = readchar.readkey()
         if key == 's':
-            urls = map(lambda res: res['href'], results)
-            url = questionary.select(
-                "Select Url: ",
-                list(urls)
-            ).ask()
+            url = pickUrl(results)
             summarizeUrl(url)
-        elif key == 'q':
+        if key == 'q':
             break
 
     status_bar.stop()
 
 def summarizeUrl(url):
-    headers = {
-        'User-Agent': fake.firefox()
-    }
-    status_bar.set("[bold blue]Fetching URL...")
-    res = requests.get(url, headers=headers)
-    status_bar.clear()
-    if res.status_code != 200:
-        console.print("[red bold]Error fetching the url[/red bold]")
-        console.print(f'[red bold]Status code:[/red bold] {res.status_code}')
-        return
-    console.print("[blue bold]Fetched Url")
-    soup = BeautifulSoup(res.text, 'html.parser')
-    for tag in soup(['script', 'style', 'noscript', 'svg', 'nav', 'footer']):
-        tag.decompose()
-
-    content = soup.get_text(separator='\n', strip=True)
+    content = fetchUrl(url)
     status_bar.set('[bold blue]Summarizing...')
     interaction = client.interactions.create(
         model = "gemini-3.8-flash",
@@ -113,10 +142,28 @@ def summarizeUrl(url):
     status_bar.clear()
     console.print(interaction.output_text)
 
+def fetchUrl(url):
+    headers = {
+        'User-Agent': fake.firefox()
+    }
+    status_bar.set("[bold blue]Fetching URL...")
+    res = requests.get(url, headers=headers)
+    status_bar.clear()
+    if res.status_code != 200:
+        console.print("[red bold]Error fetching the url[/red bold]")
+        console.print(f'[red bold]Status code:[/red bold] {res.status_code}')
+        return
+    console.print("[blue bold]Fetched Url")
+    soup = BeautifulSoup(res.text, 'html.parser')
+    for tag in soup(['script', 'style', 'noscript', 'svg', 'nav', 'footer']):
+        tag.decompose()
+    content = soup.get_text(separator='\n', strip=True)
+    return content
 
-def search(query):
+
+def search(query, max_results):
     status_bar.set("[bold blue]Searching")
-    results = DDGS().text(query, max_results=5)
+    results = DDGS().text(query, max_results=max_results)
     status_bar.clear()
     return results
 
